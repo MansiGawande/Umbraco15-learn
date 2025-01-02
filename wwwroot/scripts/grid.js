4fb9fdf6f533918533581c4d875a8aad5a0ad2f6
Mansi Gawande <mansi.gawande@softude.com>
2025-01-02 19:20:36 +0530

    02/1/2024

--- wwwroot/scripts/grid.js
+++ wwwroot/scripts/grid.js
@@ -18,14 +18,7 @@ const swiper = new Swiper('.slider-wrapper', {
   breakpoints: {
     0: {
       slidesPerView: 1
-    },
-    768: {
-      slidesPerView: 2
-    },
-    1024: {
-      slidesPerView: 3
     }
    
-
   }
 });
